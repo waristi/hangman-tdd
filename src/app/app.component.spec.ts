c6237cf465e863cc70ffa7589bf233903c24adb1
Bernardo Alexander Zuluaga Aristizabal <bernardo.az27@gmail.com>
2020-02-22 15:30:49 -0500

    Cambios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,39 +14,11 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  /*it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'hangman-tdd'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hangman-tdd');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hangman-tdd app is running!');
-  });*/
-
   it('should show the welcome message',() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.welcome span').textContent).toContain('Bienvenido al HangMan');
   });
-
-  /*it('should show the tries', ()=>{
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;    
-    expect(compiled.querySelector('.text-tries').textContent).toContain('Intentos 0 / 9');
-  });*/
-
-
-
+  
 });
